a3b382defd88f17e64773089460ee71c55e440a7
codeanticode <andres.colubri@gmail.com>
2023-12-15 01:55:46 -0500

    implemented plotting of transmission tree based on sequence data and phylogenetic tree

--- plot_sequences.py
+++ plot_sequences.py
@@ -8,6 +8,15 @@ from PIL import Image, ImageDraw, ImageFont
 import pandas as pd
 import json
 
+from Bio import Phylo
+from Bio.Phylo.TreeConstruction import DistanceCalculator
+from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
+from Bio import AlignIO
+
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.colors as clr
+
 # Load properties
 if len(sys.argv) < 1:
     print("JSON files with simulation properties are missing")
@@ -101,9 +110,15 @@ def get_prev_seq(p_mut_id, transmissions, fasta_lines):
             if not prev_seq: return []
             apply_delta(prev_seq, json.loads(prev_mutation["delta"].values[0]))
             prev_mutation["sequence"].values[0] = prev_seq
+            add_seq = True
             if pathogen_id < pp_mut_id:
-                transmissions += [(pp_mut_id - pathogen_id - 1, p_mut_id - pathogen_id - 1)]
-            add_sequence(p_mut_id, pp_mut_id, prev_seq, fasta_lines)
+                t = (pp_mut_id - pathogen_id - 1, p_mut_id - pathogen_id - 1)
+                if not t in transmissions:
+                    transmissions += [t]
+                else:
+                    print("Warning: duplicated transmission", str(pp_mut_id) + "-" + str(p_mut_id))
+                    add_seq = False
+            if add_seq: add_sequence(p_mut_id, pp_mut_id, prev_seq, fasta_lines)
             return prev_seq.copy()
 
 transmissions = []
@@ -120,15 +135,32 @@ for idx in mutations.index:
         continue
     apply_delta(prev_seq, json.loads(mutations["delta"][idx]))
     mutations.at[idx, "sequence"] = prev_seq
+    add_seq = True
     if pathogen_id < p_mut_id:
-        transmissions += [(p_mut_id - pathogen_id - 1, mut_id - pathogen_id - 1)]    
-    add_sequence(mut_id, p_mut_id, prev_seq, fasta_lines)
+        t = (p_mut_id - pathogen_id - 1, mut_id - pathogen_id - 1)
+        if not t in transmissions:
+            transmissions += [t]
+        else:
+            print("Warning: duplicated transmission", str(p_mut_id) + "-" + str(mut_id))
+            add_seq = False
+    if add_seq: add_sequence(mut_id, p_mut_id, prev_seq, fasta_lines)
 
 print("Saving FASTA file")
 fasta_fn = path.join(output_folder, "sequences.fasta")
 with open(fasta_fn, 'w') as f:
     for line in fasta_lines:
         f.write(line + '\n')
+
+print("Saving MSA file")
+# All sequences from the sim align perfectly since they only differ in point mutations
+# from one another.
+phy_fn = path.join(output_folder, "msa.phy")
+with open(phy_fn, 'w') as f:
+    f.write(str(int(len(fasta_lines)/2)) + " " + str(len(ref_seq)) + '\n')
+    for i in range(1, len(fasta_lines), 2):
+        name = fasta_lines[i - 1]
+        seq = fasta_lines[i]
+        f.write(name[4:].ljust(10) + seq + '\n')
 
 # Create network of transmissions from sequence data
 
@@ -144,5 +176,29 @@ style["vertex_label_size"] = 10
 style["vertex_label_dist"] = 1.3
 
 print("Saving network image")
-img_fn = path.join(output_folder, "transmissions.png")
+img_fn = path.join(output_folder, "transmissions.pdf")
 p = plot(g, img_fn, **style)
+
+# Generate phylogenetic tree from the sequences
+# (could also do online using the Fasta file in https://www.ebi.ac.uk/Tools/msa/clustalo/)
+
+print("Generating phylogenetic tree")
+
+aln = AlignIO.read(phy_fn, 'phylip')
+
+calculator = DistanceCalculator('identity')
+dm = calculator.get_distance(aln)
+
+constructor = DistanceTreeConstructor()
+tree = constructor.upgma(dm)
+
+tree_fn = path.join(output_folder, "tree.dnd")
+Phylo.write(tree, tree_fn, "newick")
+
+print("Saving tree image")
+img_fn = path.join(output_folder, "tree.pdf")
+
+matplotlib.rc('font', size=8)
+fig, ax = plt.subplots(figsize=(20, 70), dpi=100)
+Phylo.draw(tree, axes=ax, do_show=False)
+fig.savefig(img_fn, dpi=100)
